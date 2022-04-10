78ff7f1399ffe52d3fbf315a0584200dac81535c
BuildTools <unconfigured@null.spigotmc.org>
2022-04-10 11:40:45 +0500

    07.04

---
+++ Homework1/Homework1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+template <typename T>
+void swap(T& a, T& b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template <typename T>
+void print(T* source_begin, T* source_end)
+{
+    while (source_begin != source_end)
+    {
+        std::cout << *source_begin << ' ';
+        ++source_begin;
+    }
+    std::cout << '\n';
+}
+
+int main()
+{
+    double d = 74.9;
+    double g = 1.1;
+    swap(d, g);
+    std::cout << d << ' ' << g;
+
+    std::cout << '\n';
+
+    int arr1[3] = { 123, 3, 25 };
+    double arr2[5] = { 123.1, 3.44, 25, 1, -0.555 };
+    print(arr1, (arr1 + 3));
+    print(arr2, (arr2 + 5));
+}
+
